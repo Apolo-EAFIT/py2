4c8d661645f562ac87a518effb6a0809ef73962a
Nicolás Hock Isaza <nhocki@gmail.com>
2010-11-29 15:50:30 -0500

    HPTA!!

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -54,11 +54,7 @@ class AttachmentsController < BaseController
   def find_source
     params.each do |name, value|
       if name =~ /(.+)_id$/
-        if $1 != "user"
-          @source = $1.classify.constantize.find(value)
-        else
-          @source = $1.classify.constantize.find_by_login(value)
-        end
+        @source = $1.classify.constantize.find(value)
         return
       end
     end
